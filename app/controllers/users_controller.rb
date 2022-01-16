9edd76e4a35877c802eb0b98b1733a157b5b38d3
Kayla <luebbertkayla@gmail.com>
2022-01-16 00:10:58 -0500

    commit tree f420916a326f9abf8449d82442d266b3147adbee

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,62 @@
 class UsersController < ApplicationController
+    
+    get "/signup" do
+        if logged_in?
+            redirect "/tweets"
+        else 
+            erb :signup
+        end 
+    end
+    
+      post "/signup" do
+        if params[:username].empty?
+          redirect to '/signup'
+        end
+    
+        if params[:email].empty?
+          redirect to '/signup'
+        end
+    
+        if params[:password].empty?
+          redirect to '/signup'
+        end 
+    
+        @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+        if @user.save
+          redirect '/tweets'
+        else
+          redirect '/signup'
+        end
+      end
+    
+      get "/login" do
+        if logged_in?
+            redirect to "/tweets"
+        else 
+            erb :"users/login"
+        end 
+    end
+    
+        post "/login" do
+            @user = User.find_by(:username => params[:username])
+    
+            if @user && @user.authenticate(params[:password])
+                session[:user_id] = @user.id
+                redirect to "/tweets"
+            else 
+                redirect to "users/login"
+            end 
+        end
+    
+      get "/logout" do
+            session.clear
+            redirect to "users/login"
+        end
+    
+    get "/users/:slug" do 
+        @user = User.find_by_slug(params[:slug])
+        erb :'users/show'
+    end 
 
 
 end
